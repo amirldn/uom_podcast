4d3ce76247bce8029c9ef3077d829570520510cf
Amir Maula <amirmaula@outlook.com>
2020-05-29 17:45:55 +0100

    Update content.js
    working on method that finds the element to remove

--- content.js
+++ content.js
@@ -1,2 +1,14 @@
 // content.js
-alert("Hello from your Chrome extension!")
+// alert("Hello from your Chrome extension! v0.2")
+function pollDOM () {
+  var el = document.querySelector("#paella_plugin_PlayButtonOnScreen > canvas");
+  if (el !== null) {
+      console.log("Found: ", el);
+      el.parentElement.removeChild(el);
+    } else {
+      setTimeout(pollDOM, 300); // try again in 300 milliseconds
+      console.log("Not found element, retrying...");
+    }
+}
+
+pollDOM();
