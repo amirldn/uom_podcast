7e1e6ffc48cdad19c99b0992e4ed4a4dc4f401b8
b66957am <amir.maula@student.manchester.ac.uk>
2020-12-24 22:11:17 +0000

    Added support for blackboard and reduced CPU usage
    Removes the top title bar for the embedded blackboard podcast player
    Stopped unnecessary polls for the element once it was already removed

---
+++ builds/1.0.3/content.js
@@ -0,0 +1,44 @@
+var removed = false;
+
+function pollDOM() {
+    var isBlackboard = null;
+    if (window.location.href.includes("video.manchester.ac.uk/embedded")) {
+        isBlackboard = true;
+    } else if (window.location.href.includes("video.manchester.ac.uk/lectures") || window.location.href.includes("video.manchester.ac.uk/paella")) {
+        isBlackboard = false;
+    }
+
+    console.log("isBlackboard: " + isBlackboard)
+    if (isBlackboard == false) {
+        // Podcast Player
+        console.log("here1")
+        var el = document.querySelector("#playerContainer_videoContainer > div.play-button-on-screen > div");
+        if (el !== null) {
+            console.log("Found: ", el);
+            el.parentElement.removeChild(el);
+            var el = "REMOVED";
+            removed = true;
+            console.log("Element has now been removed.")
+        } else {
+            console.log("Not found element, retrying...");
+        }
+
+    } else {
+        // Blackboard  
+        console.log("here2");
+        var el = document.querySelector("#video > div.vjs-title-bar-link");
+        if (el !== null) {
+            console.log("Found: ", el);
+            el.parentElement.removeChild(el);
+            var el = "REMOVED";
+            removed = true;
+            console.log("Element has now been removed.")
+        } else {
+            console.log("Not found element, retrying...");
+        }
+    };
+};
+
+if (removed == false) {
+    setTimeout(pollDOM, 5000);
+}
